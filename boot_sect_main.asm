3553cf470822f32d6b488cf7f1a745fc483d3fae
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-04-12 23:20:42 -0400

    32 bit mode

--- boot_sect_main.asm
+++ boot_sect_main.asm
@@ -1,29 +1,28 @@
 [org 0x7c00]
 
-mov bp, 0x8000
+mov bp, 0x9000
 mov sp, bp
 
-mov bx, 0x9000
-mov dh, 2
+mov bx, MSG_REAL_MODE
+call print
 
-call disk_load
-
-mov dx, [0x9000]
-call print_hex
-
-call print_nl
-
-mov dx, [0x9000 + 512]
-call print_hex
-
+call switch_to_pm
 jmp $
 
 %include "boot_sect_print.asm"
-%include "boot_sect_print_hex.asm"
-%include "boot_sect_disk.asm"
+%include "32bit-gdt.asm"
+%include "32bit-print.asm"
+%include "32bit-switch.asm"
+
+[bits 32]
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm
+    jmp $
+
+MSG_REAL_MODE db "Started in 16 bit real mode", 0
+MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
 
-times 256 dw 0xdada
-times 256 dw 0xface
--- boot_sect_print.asm
+++ boot_sect_print.asm
@@ -1,31 +1,37 @@
 print:
     pusha
 
-; If current byte is 0, exit
-; Else print
-loop:
-    mov al, [bx]
-    cmp al, 0
-    je zero
+; keep this in mind:
+; while (string[i] != 0) { print string[i]; i++ }
 
+; the comparison for string end (null byte)
+start:
+    mov al, [bx] ; 'bx' is the base address for the string
+    cmp al, 0 
+    je done
+
+    ; the part where we print with the BIOS help
     mov ah, 0x0e
-    int 0x10
+    int 0x10 ; 'al' already contains the char
 
+    ; increment pointer and do next loop
     add bx, 1
-    jmp loop
+    jmp start
 
-zero:
+done:
     popa
     ret
 
+
+
 print_nl:
     pusha
-
+    
     mov ah, 0x0e
-    mov al, 0x0a  ; nl
+    mov al, 0x0a ; newline char
     int 0x10
-    mov al, 0x0d  ; cr
+    mov al, 0x0d ; carriage return
     int 0x10
-
+    
     popa
     ret
