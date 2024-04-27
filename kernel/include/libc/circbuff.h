e521e6881d74bc5d2969270c7131b28fd57eba5d
Thomas Harrison <twh2898@vt.edu>
2024-04-27 12:07:38 -0400

    Start cbuff, needs more work on insert and read

---
+++ kernel/include/libc/circbuff.h
@@ -0,0 +1,22 @@
+#ifndef CIRCBUFF_H
+#define CIRCBUFF_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct _circbuff circbuff_t;
+
+circbuff_t * circbuff_new(size_t size);
+void circbuff_free(circbuff_t * cbuff);
+
+size_t circbuff_buff_size(circbuff_t * cbuff);
+size_t circbuff_lens(circbuff_t * cbuff);
+
+uint8_t circbuff_at(circbuff_t * cbuff, size_t index);
+size_t circbuff_push(circbuff_t * cbuff, uint8_t data);
+uint8_t circbuff_pop(circbuff_t * cbuff);
+
+size_t circbuff_insert(circbuff_t * cbuff, uint8_t * data, size_t count);
+size_t circbuff_read(circbuff_t * cbuff, uint8_t * data, size_t count);
+
+#endif // CIRCBUFF_H
---
+++ kernel/src/libc/circbuff.c
@@ -0,0 +1,113 @@
+#include "libc/circbuff.h"
+
+#include "libc/mem.h"
+
+#define SAFETY 0
+
+#if SAFETY
+#define TEST_PTR(REF) \
+    if (!(REF))       \
+        return 0;
+#define TEST_MIN(REF)                  \
+    if ((REF)->len < (REF)->buff_size) \
+        return 0;
+#define TEST_MAX(REF)                   \
+    if ((REF)->len >= (REF)->buff_size) \
+        return 0;
+#else
+#define TEST_PTR(REF)
+#define TEST_MIN(REF)
+#define TEST_MAX(REF)
+#endif
+
+struct _circbuff {
+    uint8_t * buff;
+    size_t buff_size;
+    size_t start;
+    size_t len;
+};
+
+static size_t _next_index(const circbuff_t * cbuff, size_t i) {
+    TEST_PTR(cbuff)
+    while (i >= cbuff->buff_size) return i -= cbuff->buff_size;
+    return i + 1;
+}
+
+circbuff_t * circbuff_new(size_t size) {
+    if (size == 0)
+        return 0;
+
+    circbuff_t * cbuff = malloc(sizeof(circbuff_t));
+    if (cbuff) {
+        cbuff->buff = malloc(size);
+        if (!cbuff->buff) {
+            free(cbuff);
+            return 0;
+        }
+        cbuff->buff_size = size;
+        cbuff->start = 0;
+        cbuff->len = 0;
+    }
+    return cbuff;
+}
+
+void circbuff_free(circbuff_t * cbuff) {
+    if (cbuff) {
+        free(cbuff->buff);
+        free(cbuff);
+    }
+}
+
+size_t circbuff_buff_size(circbuff_t * cbuff) {
+    TEST_PTR(cbuff)
+    return cbuff->buff_size;
+}
+
+size_t circbuff_lens(circbuff_t * cbuff) {
+    TEST_PTR(cbuff)
+    return cbuff->len;
+}
+
+uint8_t circbuff_at(circbuff_t * cbuff, size_t index) {
+    TEST_PTR(cbuff)
+    TEST_MIN(cbuff)
+    return cbuff->buff[index];
+}
+
+size_t circbuff_push(circbuff_t * cbuff, uint8_t data) {
+    TEST_PTR(cbuff)
+    if (cbuff->len < cbuff->buff_size) {
+        size_t next = _next_index(cbuff, cbuff->start + cbuff->len);
+        cbuff->buff[next] = data;
+        cbuff->len++;
+        return 1;
+    }
+    return 0;
+}
+
+uint8_t circbuff_pop(circbuff_t * cbuff) {
+    TEST_PTR(cbuff)
+    if (cbuff->len > 0) {
+        uint8_t at = cbuff->buff[cbuff->start];
+        cbuff->start = _next_index(cbuff, cbuff->start);
+        return at;
+    }
+    return 0;
+}
+
+size_t circbuff_insert(circbuff_t * cbuff, uint8_t * data, size_t count) {
+    TEST_PTR(cbuff)
+    size_t o_len = 0;
+    for (size_t i = 0; i < count; i++) {
+        data[o_len++] = cbuff_at()
+    }
+    return o_len;
+}
+
+size_t circbuff_read(circbuff_t * cbuff, uint8_t * data, size_t count) {
+    TEST_PTR(cbuff)
+    size_t o_len = 0;
+    for (size_t i = 0; i < count; i++) {
+    }
+    return o_len;
+}
