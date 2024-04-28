b1d46351d372914b2586d190457d41079a0c027f
Thomas Harrison <twh2898@vt.edu>
2024-04-27 20:35:12 -0400

    Flip read and insert

--- kernel/src/libc/circbuff.c
+++ kernel/src/libc/circbuff.c
@@ -108,8 +108,8 @@ size_t circbuff_insert(circbuff_t * cbuff, uint8_t * data, size_t count) {
     TEST_PTR(cbuff)
     size_t o_len = 0;
     for (size_t i = 0; i < count; i++) {
-        if (cbuff->len > 0)
-            data[o_len++] = circbuff_pop(cbuff);
+        if (cbuff->len < cbuff->buff_size)
+            circbuff_push(cbuff, data[o_len++]);
     }
     return o_len;
 }
@@ -118,8 +118,8 @@ size_t circbuff_read(circbuff_t * cbuff, uint8_t * data, size_t count) {
     TEST_PTR(cbuff)
     size_t o_len = 0;
     for (size_t i = 0; i < count; i++) {
-        if (cbuff->len < cbuff->buff_size)
-            circbuff_push(cbuff, data[o_len++]);
+        if (cbuff->len > 0)
+            data[o_len++] = circbuff_pop(cbuff);
     }
     return o_len;
 }
