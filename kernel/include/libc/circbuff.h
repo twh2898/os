4200ca734634996cea42f745b64e473b61902c1f
Thomas Harrison <twh2898@vt.edu>
2024-05-04 02:04:12 -0400

    Add docstrings

--- kernel/include/libc/circbuff.h
+++ kernel/include/libc/circbuff.h
@@ -8,9 +8,9 @@ typedef struct _circbuff circbuff_t;
 
 /**
  * @brief Create an empty circular buffer with given maximum size.
- * 
+ *
  * Will return null if size is 0.
- * 
+ *
  * @param size maximum buffer size
  * @return circbuff_t* new buffer
  */
@@ -18,7 +18,7 @@ circbuff_t * circbuff_new(size_t size);
 
 /**
  * @brief Free a circular buffer.
- * 
+ *
  * @pre cbuff is not null and is a valid circular buffer
  * @param cbuff buffer
  */
@@ -26,9 +26,9 @@ void circbuff_free(circbuff_t * cbuff);
 
 /**
  * @brief Get the maximum length of the buffer.
- * 
+ *
  * This is the size given to circbuff_new
- * 
+ *
  * @param cbuff buffer
  * @return size_t max length of buffer
  */
@@ -36,7 +36,7 @@ size_t circbuff_buff_size(const circbuff_t * cbuff);
 
 /**
  * @brief Get the current buffer length.
- * 
+ *
  * @param cbuff buffer
  * @return size_t number of elements in the buffer
  */
@@ -44,9 +44,9 @@ size_t circbuff_len(const circbuff_t * cbuff);
 
 /**
  * @brief Get element at index
- * 
+ *
  * If the buffer is empty, return 0.
- * 
+ *
  * @param cbuff buffer
  * @param index index from start
  * @return uint8_t value at index
@@ -55,9 +55,9 @@ uint8_t circbuff_at(const circbuff_t * cbuff, size_t index);
 
 /**
  * @brief Add an element to the end of the buffer.
- * 
+ *
  * If the buffer is full, return 0.
- * 
+ *
  * @param cbuff buffer
  * @param data element to append
  * @return size_t 1 if data is appended, 0 if buffer is full
@@ -66,18 +66,60 @@ size_t circbuff_push(circbuff_t * cbuff, uint8_t data);
 
 /**
  * @brief Remove and return an element from the start of the buffer.
- * 
+ *
  * If the buffer is empty, return 0.
- * 
+ *
  * @param cbuff buffer
  * @return uint8_t element or 0 if buffer is empty
  */
 uint8_t circbuff_pop(circbuff_t * cbuff);
 
+/**
+ * @brief Insert count elements from data into the buffer.
+ *
+ * If count is > the remaining space, return the number of elements added from
+ * data before the buffer was full. If the buffer is already full, return 0.
+ *
+ * @param cbuff buffer
+ * @param data elements to add
+ * @param count size of data
+ * @return size_t number of elements added <= count
+ */
 size_t circbuff_insert(circbuff_t * cbuff, uint8_t * data, size_t count);
+
+/**
+ * @brief Read up to count elements from the buffer to data.
+ *
+ * If count is > the buffer length, read only up to return the buffer length.
+ *
+ * @param cbuff buffer
+ * @param data output elements
+ * @param count number of elements to read
+ * @return size_t number of elements read into data
+ */
 size_t circbuff_read(const circbuff_t * cbuff, uint8_t * data, size_t count);
+
+/**
+ * @brief Read and remove count elements from the buffer.
+ *
+ * If data is null, skip reading the data before removing it. If count is >
+ * the length of the buffer, return the length of the buffer.
+ *
+ * @param cbuff buffer
+ * @param data null or pointer to read data into
+ * @param count number of elements to read and remove
+ * @return size_t number of elements removed
+ */
 size_t circbuff_remove(circbuff_t * cbuff, uint8_t * data, size_t count);
 
+/**
+ * @brief Clear all elements and set the length to 0.
+ *
+ * If length is already 0, this function has no side affects and will return 0.
+ *
+ * @param cbuff buffer
+ * @return size_t number of items removed
+ */
 size_t circbuff_clear(circbuff_t * cbuff);
 
 #endif // CIRCBUFF_H
