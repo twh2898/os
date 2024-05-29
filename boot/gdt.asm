de41011a29619c836ccc0db8614b14e7834aeb6a
Thomas Harrison <twh2898@vt.edu>
2024-05-28 20:33:04 -0400

    Updated gdt for kernel and user mode

--- boot/gdt.asm
+++ boot/gdt.asm
@@ -1,27 +1,43 @@
+align 4
 gdt_start: ; don't remove the labels, they're needed to compute sizes and jumps
-    ; the GDT starts with a null 8-byte
+
+gdt_null:
     dd 0x0 ; 4 byte
     dd 0x0 ; 4 byte
 
-; GDT for code segment. base = 0x00000000, length = 0xfffff
-; for flags, refer to os-dev.pdf document, page 36
-gdt_code: 
+gdt_code:
+gdt_kernel_code:
     dw 0xffff    ; segment length, bits 0-15
-    dw 0x0       ; segment base, bits 0-15
-    db 0x0       ; segment base, bits 16-23
-    db 10011010b ; flags (8 bits)
+    dw 0x0000    ; segment base, bits 0-15
+    db 0x00      ; segment base, bits 16-23
+    db 10011010b ; access byte / flags (8 bits)
     db 11001111b ; flags (4 bits) + segment length, bits 16-19
-    db 0x0       ; segment base, bits 24-31
+    db 0x00      ; segment base, bits 24-31
 
-; GDT for data segment. base and length identical to code segment
-; some flags changed, again, refer to os-dev.pdf
 gdt_data:
-    dw 0xffff
-    dw 0x0
-    db 0x0
-    db 10010010b
-    db 11001111b
-    db 0x0
+gdt_kernel_data:
+    dw 0xffff    ; segment length, bits 0-15
+    dw 0x0000    ; segment base, bits 0-15
+    db 0x00      ; segment base, bits 16-23
+    db 10010010b ; access byte / flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+    db 0x00      ; segment base, bits 24-31
+
+gdt_user_code:
+    dw 0xffff    ; segment length, bits 0-15
+    dw 0x0000    ; segment base, bits 0-15
+    db 0x00      ; segment base, bits 16-23
+    db 11111010b ; access byte / flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+    db 0x00      ; segment base, bits 24-31
+
+gdt_user_data:
+    dw 0xffff    ; segment length, bits 0-15
+    dw 0x0000    ; segment base, bits 0-15
+    db 0x00      ; segment base, bits 16-23
+    db 11110010b ; access byte / flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+    db 0x00      ; segment base, bits 24-31
 
 gdt_end:
 
