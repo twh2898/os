69963f625928e52e969bb3a67ec7409fb38c4baf
Thomas Harrison <twh2898@vt.edu>
2024-06-12 21:01:38 -0400

    Updated mmu

--- kernel/include/cpu/mmu.h
+++ kernel/include/cpu/mmu.h
@@ -8,26 +8,29 @@
 #define PAGE_DIR_SIZE 1024
 #define PAGE_TABLE_SIZE 1024
 
-enum MMU_PAGE_DIR_FLAGS {
-    MMU_PAGE_DIR_FLAGS_PRESENT = 0x1,
-    MMU_PAGE_DIR_FLAGS_READ_WRITE = 0x2,
-    MMU_PAGE_DIR_FLAGS_USER_SUPERVISOR = 0x4,
-    MMU_PAGE_DIR_FLAGS_WRITE_THROUGH = 0x8,
-    MMU_PAGE_DIR_FLAGS_CACHE_DISABLE = 0x10,
-    MMU_PAGE_DIR_FLAGS_ACCESSED = 0x20,
-    MMU_PAGE_DIR_FLAGS_PAGE_SIZE = 0x80,
+#define MMU_DIR_RW (MMU_PAGE_DIR_FLAG_PRESENT | MMU_PAGE_DIR_FLAG_READ_WRITE)
+#define MMU_TABLE_RW (MMU_PAGE_TABLE_FLAG_PRESENT | MMU_PAGE_TABLE_FLAG_READ_WRITE)
+
+enum MMU_PAGE_DIR_FLAG {
+    MMU_PAGE_DIR_FLAG_PRESENT = 0x1,
+    MMU_PAGE_DIR_FLAG_READ_WRITE = 0x2,
+    MMU_PAGE_DIR_FLAG_USER_SUPERVISOR = 0x4,
+    MMU_PAGE_DIR_FLAG_WRITE_THROUGH = 0x8,
+    MMU_PAGE_DIR_FLAG_CACHE_DISABLE = 0x10,
+    MMU_PAGE_DIR_FLAG_ACCESSED = 0x20,
+    MMU_PAGE_DIR_FLAG_PAGE_SIZE = 0x80,
 };
 
-enum MMU_PAGE_TABLE_FLAGS {
-    MMU_PAGE_TABLE_FLAGS_PRESENT = 0x1,
-    MMU_PAGE_TABLE_FLAGS_READ_WRITE = 0x2,
-    MMU_PAGE_TABLE_FLAGS_USER_SUPERVISOR = 0x4,
-    MMU_PAGE_TABLE_FLAGS_WRITE_THROUGH = 0x8,
-    MMU_PAGE_TABLE_FLAGS_CACHE_DISABLE = 0x10,
-    MMU_PAGE_TABLE_FLAGS_ACCESSED = 0x20,
-    MMU_PAGE_TABLE_FLAGS_DIRTY = 0x40,
-    MMU_PAGE_TABLE_FLAGS_PAT = 0x80,
-    MMU_PAGE_TABLE_FLAGS_GLOBAL = 0x100,
+enum MMU_PAGE_TABLE_FLAG {
+    MMU_PAGE_TABLE_FLAG_PRESENT = 0x1,
+    MMU_PAGE_TABLE_FLAG_READ_WRITE = 0x2,
+    MMU_PAGE_TABLE_FLAG_USER_SUPERVISOR = 0x4,
+    MMU_PAGE_TABLE_FLAG_WRITE_THROUGH = 0x8,
+    MMU_PAGE_TABLE_FLAG_CACHE_DISABLE = 0x10,
+    MMU_PAGE_TABLE_FLAG_ACCESSED = 0x20,
+    MMU_PAGE_TABLE_FLAG_DIRTY = 0x40,
+    MMU_PAGE_TABLE_FLAG_PAT = 0x80,
+    MMU_PAGE_TABLE_FLAG_GLOBAL = 0x100,
 };
 
 typedef uint32_t mmu_page_entry_t;
@@ -38,19 +41,23 @@ typedef struct {
 
 typedef mmu_page_dir_t mmu_page_table_t;
 
-mmu_page_dir_t * mmu_dir_create();
+mmu_page_dir_t * mmu_dir_create(void * addr);
 void mmu_dir_free(mmu_page_dir_t * dir);
 
-mmu_page_table_t * mmu_table_create();
+mmu_page_table_t * mmu_table_create(void * addr);
 void mmu_table_free(mmu_page_table_t * table);
 
 void mmu_dir_set_table(mmu_page_dir_t * dir, size_t i, mmu_page_table_t * table);
-void mmu_dir_set_flags(mmu_page_dir_t * dir, size_t i, enum MMU_PAGE_DIR_FLAGS flags);
+void mmu_dir_set_flags(mmu_page_dir_t * dir, size_t i, enum MMU_PAGE_DIR_FLAG flags);
+mmu_page_table_t * mmu_dir_get_table(mmu_page_dir_t * dir, size_t i);
 
 void mmu_table_set_addr(mmu_page_table_t * table, size_t i, uint32_t page_addr);
 void mmu_table_set_flags(mmu_page_table_t * table,
                          size_t i,
-                         enum MMU_PAGE_TABLE_FLAGS flags);
+                         enum MMU_PAGE_TABLE_FLAG flags);
+
+uint32_t mmu_table_get_addr(mmu_page_table_t * table, size_t i);
+enum MMU_PAGE_TABLE_FLAG mmu_table_get_flags(mmu_page_table_t * table, size_t i);
 
 extern void mmu_enable_paging(mmu_page_dir_t * dir);
 extern void mmu_disable_paging();
--- kernel/src/cpu/mmu.c
+++ kernel/src/cpu/mmu.c
@@ -7,14 +7,17 @@
 #include "libc/string.h"
 
 #define PAGE_SIZE 0x1000 // 4k (>> 12)
-#define PAGE_ALIGNED_DOWN(PTR) ((PTR) & 0xfffff000)
+#define PAGE_ALIGNED_DOWN(PTR) ((PTR) & MASK_ADDR)
 #define PAGE_ALIGNED_UP(PTR) ((PAGE_ALIGNED_DOWN(PTR)) + PAGE_SIZE)
-#define PAGE_ALIGNED(PTR) (((PTR) & 0xfff) ? PAGE_ALIGNED_UP(PTR) : (PTR))
+#define PAGE_ALIGNED(PTR) (((PTR) & MASK_FLAGS) ? PAGE_ALIGNED_UP(PTR) : (PTR))
+
+#define MASK_ADDR 0xfffff000
+#define MASK_FLAGS 0xfff
 
 // mmu_page_dir_t * identity_dir = 0x1000;
 
-mmu_page_dir_t * mmu_dir_create() {
-    mmu_page_dir_t * dir = page_alloc();
+mmu_page_dir_t * mmu_dir_create(void * addr) {
+    mmu_page_dir_t * dir = addr;
     if (dir) {
         memset(dir, 0, sizeof(mmu_page_dir_t));
     }
@@ -22,10 +25,13 @@ mmu_page_dir_t * mmu_dir_create() {
 }
 
 void mmu_dir_free(mmu_page_dir_t * dir) {
+    if (!dir)
+        return;
+
     if (dir) {
         for (size_t i = 0; i < PAGE_DIR_SIZE; i++) {
-            if (dir->entries[i] & MMU_PAGE_DIR_FLAGS_PRESENT) {
-                uint32_t table_addr = dir->entries[i] & 0xfffff000;
+            if (dir->entries[i] & MMU_PAGE_DIR_FLAG_PRESENT) {
+                uint32_t table_addr = dir->entries[i] & MASK_ADDR;
                 mmu_table_free((mmu_page_table_t *)table_addr);
             }
         }
@@ -33,8 +39,8 @@ void mmu_dir_free(mmu_page_dir_t * dir) {
     page_free(dir);
 }
 
-mmu_page_table_t * mmu_table_create() {
-    mmu_page_table_t * table = page_alloc();
+mmu_page_table_t * mmu_table_create(void * addr) {
+    mmu_page_table_t * table = addr;
     if (table) {
         memset(table, 0, sizeof(mmu_page_table_t));
     }
@@ -42,10 +48,13 @@ mmu_page_table_t * mmu_table_create() {
 }
 
 void mmu_table_free(mmu_page_table_t * table) {
+    if (!table)
+        return;
+
     if (table) {
         for (size_t i = 0; i < PAGE_TABLE_SIZE; i++) {
-            if (table->entries[i] & MMU_PAGE_TABLE_FLAGS_PRESENT) {
-                uint32_t page_addr = table->entries[i] & 0xfffff000;
+            if (table->entries[i] & MMU_PAGE_TABLE_FLAG_PRESENT) {
+                uint32_t page_addr = table->entries[i] & MASK_ADDR;
                 page_free((void *)page_addr);
             }
         }
@@ -54,64 +63,87 @@ void mmu_table_free(mmu_page_table_t * table) {
 }
 
 void mmu_dir_set_table(mmu_page_dir_t * dir, size_t i, mmu_page_table_t * table) {
-    if (i >= PAGE_DIR_SIZE)
+    if (!dir || !table || i >= PAGE_DIR_SIZE)
         return;
 
     mmu_page_entry_t entry = dir->entries[i];
-    entry &= 0xfff;
-    entry |= ((uint32_t)table) & 0xfffff000;
+    entry &= MASK_FLAGS;
+    entry |= ((uint32_t)table) & MASK_ADDR;
 
     dir->entries[i] = entry;
 }
 
-void mmu_dir_set_flags(mmu_page_dir_t * dir, size_t i, enum MMU_PAGE_DIR_FLAGS flags) {
-    if (i >= PAGE_DIR_SIZE)
+void mmu_dir_set_flags(mmu_page_dir_t * dir, size_t i, enum MMU_PAGE_DIR_FLAG flags) {
+    if (!dir || i >= PAGE_DIR_SIZE)
         return;
 
     mmu_page_entry_t entry = dir->entries[i];
-    entry &= 0xfffff000;
-    entry |= flags & 0xfff;
+    entry &= MASK_ADDR;
+    entry |= flags & MASK_FLAGS;
 
     dir->entries[i] = entry;
+}
+
+mmu_page_table_t * mmu_dir_get_table(mmu_page_dir_t * dir, size_t i) {
+    if (!dir || i >= PAGE_DIR_SIZE)
+        return 0;
+
+    mmu_page_entry_t table_entry = dir->entries[i];
+    uint32_t table_addr = table_addr & MASK_ADDR;
+    return (mmu_page_table_t *)table_addr;
 }
 
 void mmu_table_set_addr(mmu_page_table_t * table, size_t i, uint32_t page_addr) {
-    if (i >= PAGE_TABLE_SIZE)
+    if (!table || i >= PAGE_TABLE_SIZE)
         return;
 
     mmu_page_entry_t entry = table->entries[i];
-    entry &= 0xfff;
-    entry |= page_addr & 0xfffff000;
+    entry &= MASK_FLAGS;
+    entry |= page_addr & MASK_ADDR;
 
     table->entries[i] = entry;
 }
 
 void mmu_table_set_flags(mmu_page_table_t * table,
                          size_t i,
-                         enum MMU_PAGE_TABLE_FLAGS flags) {
-    if (i >= PAGE_TABLE_SIZE)
+                         enum MMU_PAGE_TABLE_FLAG flags) {
+    if (!table || i >= PAGE_TABLE_SIZE)
         return;
 
     mmu_page_entry_t entry = table->entries[i];
-    entry &= 0xfffff000;
-    entry |= flags & 0xfff;
+    entry &= MASK_ADDR;
+    entry |= flags & MASK_FLAGS;
 
     table->entries[i] = entry;
+}
+
+uint32_t mmu_table_get_addr(mmu_page_table_t * table, size_t i) {
+    if (!table || i >= PAGE_TABLE_SIZE)
+        return 0;
+
+    return table->entries[i] & MASK_ADDR;
+}
+
+enum MMU_PAGE_TABLE_FLAG mmu_table_get_flags(mmu_page_table_t * table, size_t i) {
+    if (!table || i >= PAGE_TABLE_SIZE)
+        return 0;
+
+    return table->entries[i] & MASK_FLAGS;
 }
 
 // void * mmu_phys_addr(void * virt_addr) {
 //     unsigned long pdindex = (unsigned long)virt_addr >> 22;
 
 //     // TODO THIS NEEDS TO BE WHERE THE PAGE DIR GETS MAPPED TO IN PAGING
-//     unsigned long * pd = (unsigned long *)0xFFFFF000;
+//     unsigned long * pd = (unsigned long *)MASK_ADDR;
 //     // TODO Here you need to check whether the PD entry is present.
 
 //     // TODO THIS NEEDS TO BE WHERE THE PAGE TABLE GETS MAPPED TO IN PAGING
 //     unsigned long * pt = ((unsigned long *)0xFFC00000) + (0x400 * pdindex);
 //     // TODO Here you need to check whether the PT entry is present.
 
-//     return (void *)((pt[ptindex] & ~0xFFF) + ((unsigned long)virt_addr &
-//     0xFFF));
+//     return (void *)((pt[ptindex] & ~MASK_FLAGS) + ((unsigned long)virt_addr &
+//     MASK_FLAGS));
 // }
 
 // void mmu_map_page(mmu_page_dir_t * dir,
@@ -138,8 +170,8 @@ void mmu_table_set_flags(mmu_page_table_t * table,
 //     // Here you need to check whether the PT entry is present.
 //     // When it is, then there is already a mapping present. What do you do now?
 
-//     pt[ptindex] = ((unsigned long)phys_addr) | (flags & 0xFFF) | 0x01; //
-//     Present
+//     pt[ptindex] = ((unsigned long)phys_addr) | (flags & MASK_FLAGS) | 0x01;
+//     // Present
 
 //     // Now you need to flush the entry in the TLB
 //     // or you might not notice the change.
