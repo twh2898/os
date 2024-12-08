b207c5f3580d134e63e606f62be3f8c95bf8ce00
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-12-08 16:18:32 -0500

    cleanup

--- src/apps/libc/src/interrupt.asm
+++ src/apps/libc/src/interrupt.asm
@@ -1,16 +1,16 @@
 [bits 32]
 
-global apps_send_interrupt
+global send_interrupt
 
-apps_send_interrupt:
+send_interrupt:
     push eax
     push ebx
-    
+
     mov eax, [esp+8]
     mov ebx, [esp+12]
-    
+
     int 48
-    
+
     pop ebx
     pop eax
 
--- src/apps/libc/src/sys_call.c
+++ src/apps/libc/src/sys_call.c
@@ -2,12 +2,22 @@
 
 #include <stdint.h>
 
-extern void apps_send_interrupt(uint16_t int_no, ...);
+#define SYS_INT_IO_OPEN  0x0100
+#define SYS_INT_IO_CLOSE 0x0101
+#define SYS_INT_IO_READ  0x0102
+#define SYS_INT_IO_WRITE 0x0103
+
+#define SYS_INT_MEM_MALLOC  0x0200
+#define SYS_INT_MEM_CALLOC  0x0201
+#define SYS_INT_MEM_REALLOC 0x0202
+#define SYS_INT_MEM_FREE    0x0203
+
+extern void send_interrupt(uint16_t int_no, ...);
 
 void sys_call_1() {
-    apps_send_interrupt(14);
+    send_interrupt(14);
 }
 
 void sys_call_print(char * str) {
-    apps_send_interrupt(15, str);
+    send_interrupt(15, str);
 }
