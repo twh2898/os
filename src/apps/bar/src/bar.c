c02c1d7837102eb7074c597c0432a5f902de1a77
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-12-09 21:11:06 -0500

    Pass arguments to bar

--- src/apps/bar/src/bar.c
+++ src/apps/bar/src/bar.c
@@ -1,7 +1,16 @@
+#include <stddef.h>
+#include <stdint.h>
 
-void bar() {
-}
+#include "libc/stdio.h"
 
-void __start() {
-    bar();
+int __start(size_t argc, char ** argv) {
+    kprintf("Bar got %u arguments\n", argc);
+
+    for (size_t i = 0; i < argc; i++) {
+        kprintf("Argument %u is ", i);
+        kputs(argv[i]);
+        kputc('\n');
+    }
+
+    return 0;
 }
