a01e27ce88449f5ef67546786d4cc8c44898a696
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-12-22 10:36:18 -0500

    Remove duplicate arg

--- src/drivers/src/lib/include/_driver/fs_file.h
+++ src/drivers/src/lib/include/_driver/fs_file.h
@@ -16,7 +16,7 @@ typedef struct _driver_fs_file {
 } driver_fs_file_t;
 
 typedef driver_fs_file_t * (*driver_fs_fn_file_open)(struct _driver_fs *, const char * path);
-typedef int (*driver_fs_fn_file_close)(driver_fs_file_t *, driver_fs_file_t *);
+typedef int (*driver_fs_fn_file_close)(driver_fs_file_t *);
 typedef int (*driver_fs_fn_file_stat)(driver_fs_file_t *, driver_fs_file_stat_t *);
 typedef int (*driver_fs_fn_file_read)(driver_fs_file_t *, char * buff, size_t count, size_t addr);
 typedef int (*driver_fs_fn_file_write)(driver_fs_file_t *, const char * buff, size_t count, size_t addr);
