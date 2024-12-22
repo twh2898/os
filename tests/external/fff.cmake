2da1301a9ba20e0374102539cf4c667874bf605c
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-12-21 22:19:09 -0500

    Fix fff target import

--- tests/external/fff.cmake
+++ tests/external/fff.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
 	fff
 	GIT_REPOSITORY https://github.com/meekrosoft/fff.git
-	GIT_TAG		   v1.1
+	GIT_TAG		   5111c61e1ef7848e3afd3550044a8cf4405f4199
 )
 
 FetchContent_MakeAvailable(fff)
