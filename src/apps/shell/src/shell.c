0eea3aa07df5346b6218770fda3c6e4d8fca6d4f
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-12-24 17:21:48 -0500

    Move __start to top of file

--- src/apps/shell/src/shell.c
+++ src/apps/shell/src/shell.c
@@ -1,12 +1,29 @@
 #include "libc/memory.h"
+#include "libc/signal.h"
 #include "libc/stdio.h"
 
+void foo_callback();
+
+static int i;
+
 int __start(size_t argc, char ** argv) {
+    i = 0;
+    register_signal(PROC_SIGNALS_FOO, foo_callback);
+
     puts("Welcome to shell!\n$ ");
 
     for (;;) {
+        if (i) {
+            i--;
+            puts("YAY!\n");
+        }
         asm("hlt");
     }
 
     return 0;
+}
+
+void foo_callback() {
+    puts("Got a shell callback!\n");
+    i++;
 }
