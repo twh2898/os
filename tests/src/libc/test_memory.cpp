b970370f8d4f3b793eebe203c0fc5369fde9b753
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-01-02 07:40:04 -0500

    Update libc and libk tests

--- tests/src/libc/test_memory.cpp
+++ tests/src/libc/test_memory.cpp
@@ -6,67 +6,75 @@
 
 extern "C" {
 #include "libc/memory.h"
+#include "memory_alloc.h"
 
-void * kmemset(void * ptr, uint8_t value, size_t n) {
-    return memset(ptr, value, n);
-}
+FAKE_VALUE_FUNC(void *, _page_alloc, size_t);
+FAKE_VALUE_FUNC(void *, kmemset, void *, uint8_t, size_t);
+FAKE_VALUE_FUNC(int, memory_init, memory_t *, memory_alloc_pages_t);
+FAKE_VALUE_FUNC(void *, memory_alloc, memory_t *, size_t);
+FAKE_VALUE_FUNC(void *, memory_realloc, memory_t *, void *, size_t);
+FAKE_VALUE_FUNC(int, memory_free, memory_t *, void *);
 
-FAKE_VALUE_FUNC(void *, _malloc, size_t);
-FAKE_VALUE_FUNC(void *, _realloc, void *, size_t);
-FAKE_VOID_FUNC(_free, void *);
-
-void * custom_malloc(size_t size) {
+void * custom_malloc(memory_t * memory, size_t size) {
     return malloc(size);
-}
-
-void * custom_realloc(void * ptr, size_t size) {
-    return realloc(ptr, size);
-}
-
-void custom_free(void * ptr) {
-    free(ptr);
 }
 }
 
 class LibC : public ::testing::Test {
 protected:
     void SetUp() override {
-        RESET_FAKE(_malloc);
-        RESET_FAKE(_realloc);
-        RESET_FAKE(_free);
-
-        _malloc_fake.custom_fake  = custom_malloc;
-        _realloc_fake.custom_fake = custom_realloc;
-        _free_fake.custom_fake    = custom_free;
+        RESET_FAKE(_page_alloc);
+        RESET_FAKE(kmemset);
+        RESET_FAKE(memory_init);
+        RESET_FAKE(memory_alloc);
+        RESET_FAKE(memory_realloc);
+        RESET_FAKE(memory_free);
     }
 };
 
 TEST_F(LibC, kmalloc) {
     void * ptr = kmalloc(12);
-    EXPECT_EQ(_malloc_fake.call_count, 1);
-    EXPECT_EQ(_malloc_fake.arg0_val, 12);
-    free(ptr);
+    EXPECT_EQ(1, memory_alloc_fake.call_count);
+    EXPECT_NE(nullptr, memory_alloc_fake.arg0_val);
+    EXPECT_EQ(12, memory_alloc_fake.arg1_val);
+
+    EXPECT_EQ(1, memory_init_fake.call_count);
+    EXPECT_NE(nullptr, memory_init_fake.arg0_val);
 }
 
 TEST_F(LibC, kcalloc) {
+    char buff[12];
+    memory_alloc_fake.return_val = buff;
+
     void * ptr = kcalloc(12, 0);
-    EXPECT_EQ(_malloc_fake.call_count, 1);
-    EXPECT_EQ(_malloc_fake.arg0_val, 12);
-    free(ptr);
+    EXPECT_EQ(1, memory_alloc_fake.call_count);
+    EXPECT_NE(nullptr, memory_alloc_fake.arg0_val);
+    EXPECT_EQ(12, memory_alloc_fake.arg1_val);
+
+    EXPECT_EQ(1, kmemset_fake.call_count);
+    EXPECT_EQ(buff, kmemset_fake.arg0_val);
+    EXPECT_EQ(0, kmemset_fake.arg1_val);
+    EXPECT_EQ(12, kmemset_fake.arg2_val);
 }
 
 TEST_F(LibC, krealloc) {
-    void * ptr = malloc(5);
-    ptr        = krealloc(ptr, 12);
-    EXPECT_EQ(_realloc_fake.call_count, 1);
-    EXPECT_EQ(_realloc_fake.arg0_val, ptr);
-    EXPECT_EQ(_realloc_fake.arg1_val, 12);
-    free(ptr);
+    char buff[12];
+
+    void * ptr = krealloc(buff, 12);
+    EXPECT_EQ(1, memory_realloc_fake.call_count);
+    EXPECT_NE(nullptr, memory_realloc_fake.arg0_val);
+    EXPECT_EQ(buff, memory_realloc_fake.arg1_val);
+    EXPECT_EQ(12, memory_realloc_fake.arg2_val);
+
+    EXPECT_EQ(1, memory_init_fake.call_count);
+    EXPECT_NE(nullptr, memory_init_fake.arg0_val);
 }
 
 TEST_F(LibC, kfree) {
-    _free_fake.custom_fake = nullptr;
-    _free((void *)5);
-    EXPECT_EQ(_free_fake.call_count, 1);
-    EXPECT_EQ(_free_fake.arg0_val, (void *)5);
+    char buff[12];
+    kfree(buff);
+
+    EXPECT_EQ(1, memory_free_fake.call_count);
+    EXPECT_NE(nullptr, memory_free_fake.arg0_val);
+    EXPECT_EQ(buff, memory_free_fake.arg1_val);
 }
--- tests/src/libk/test_libk.cpp
+++ tests/src/libk/test_libk.cpp
@@ -23,31 +23,31 @@ protected:
     }
 };
 
-TEST_F(LibK, malloc) {
-    send_interrupt_fake.return_val = 3;
-    void * ptr                     = _malloc(12);
-    EXPECT_EQ(send_interrupt_fake.call_count, 1);
-    EXPECT_EQ(send_interrupt_fake.arg0_val, 0x200);
-    EXPECT_EQ(send_interrupt_fake.arg1_val, 12);
-    EXPECT_EQ(ptr, (void *)3);
-}
+// TEST_F(LibK, malloc) {
+//     send_interrupt_fake.return_val = 3;
+//     void * ptr                     = _malloc(12);
+//     EXPECT_EQ(send_interrupt_fake.call_count, 1);
+//     EXPECT_EQ(send_interrupt_fake.arg0_val, 0x200);
+//     EXPECT_EQ(send_interrupt_fake.arg1_val, 12);
+//     EXPECT_EQ(ptr, (void *)3);
+// }
 
-TEST_F(LibK, realloc) {
-    send_interrupt_fake.return_val = 3;
-    void * ptr                     = _realloc((void *)5, 12);
-    EXPECT_EQ(send_interrupt_fake.call_count, 1);
-    EXPECT_EQ(send_interrupt_fake.arg0_val, 0x201);
-    EXPECT_EQ(send_interrupt_fake.arg1_val, 5);
-    EXPECT_EQ(send_interrupt_fake.arg2_val, 12);
-    EXPECT_EQ(ptr, (void *)3);
-}
+// TEST_F(LibK, realloc) {
+//     send_interrupt_fake.return_val = 3;
+//     void * ptr                     = _realloc((void *)5, 12);
+//     EXPECT_EQ(send_interrupt_fake.call_count, 1);
+//     EXPECT_EQ(send_interrupt_fake.arg0_val, 0x201);
+//     EXPECT_EQ(send_interrupt_fake.arg1_val, 5);
+//     EXPECT_EQ(send_interrupt_fake.arg2_val, 12);
+//     EXPECT_EQ(ptr, (void *)3);
+// }
 
-TEST_F(LibK, free) {
-    _free((void *)5);
-    EXPECT_EQ(send_interrupt_fake.call_count, 1);
-    EXPECT_EQ(send_interrupt_fake.arg0_val, 0x202);
-    EXPECT_EQ(send_interrupt_fake.arg1_val, 5);
-}
+// TEST_F(LibK, free) {
+//     _free((void *)5);
+//     EXPECT_EQ(send_interrupt_fake.call_count, 1);
+//     EXPECT_EQ(send_interrupt_fake.arg0_val, 0x202);
+//     EXPECT_EQ(send_interrupt_fake.arg1_val, 5);
+// }
 
 TEST_F(LibK, exit) {
     _proc_exit(200);
