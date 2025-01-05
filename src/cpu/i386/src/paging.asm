334cdb8111f01e9a5b2b34b20c47c4759b2d98d9
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-01-05 00:38:07 -0500

    Fix args to mmu paging assembly functions

--- src/cpu/i386/src/paging.asm
+++ src/cpu/i386/src/paging.asm
@@ -1,49 +1,63 @@
-; eax = page dir 4 kb aligned
+; void mmu_enable_paging(mmu_dir_t * dir);
 global mmu_enable_paging
 mmu_enable_paging:
-    pusha
+    push eax
+    mov  eax, [esp-4]
+
+    push eax
 
     call mmu_change_dir
 
     mov eax, cr0
-    or eax, 0x80000000
+    or  eax, 0x80000000
     mov cr0, eax
 
-    popa
+    pop eax
+    pop eax
+
     ret
 
+; void mmu_disable_paging();
 global mmu_disable_paging
 mmu_disable_paging:
-    pusha
+    push eax
 
     mov eax, cr0
     and eax, 0x7fffffff
     mov cr0, eax
 
-    popa
+    pop eax
+
     ret
 
+; void mmu_paging_enabled();
 global mmu_paging_enabled
 mmu_paging_enabled:
+    push eax
+
     mov eax, cr0
     shr eax, 31
     and eax, 1
 
+    pop eax
+
     ret
 
-; void mmu_change_dir(mmu_dir_t * dir)
+; void mmu_change_dir(mmu_dir_t * dir);
 global mmu_change_dir
 mmu_change_dir:
-    pusha
+    push eax
+    mov  eax, [esp-4]
 
     shr eax, 12
     shl eax, 12
     mov cr3, eax
 
-    popa
+    pop eax
+
     ret
 
-; mmu_dir_t * mmu_get_curr_dir()
+; mmu_dir_t * mmu_get_curr_dir();
 global mmu_get_curr_dir
 mmu_get_curr_dir:
     mov eax, cr3
