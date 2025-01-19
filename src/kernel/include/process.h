ad70e9afd28b112bd20f28c065a4e82ed37c69cf
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-01-19 12:08:32 -0500

    cleanup

--- src/kernel/include/process.h
+++ src/kernel/include/process.h
@@ -70,11 +70,4 @@ typedef struct _proc_man {
     process_t * curr_task;
 } proc_man_t;
 
-proc_man_t * proc_man_new();
-
-void proc_man_set_idle(proc_man_t * pm, process_t * proc);
-void proc_man_set_new_idle(proc_man_t * pm, uint32_t eip, uint32_t esp, uint32_t cr3, uint32_t ss0);
-int  proc_man_task_from_ptr(void * fn);
-void proc_man_switch_to_idle(proc_man_t * pm);
-
 #endif // KERNEL_PROCESS_H
