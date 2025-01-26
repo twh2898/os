99318d98b197c95e6a53f53cd91fca7abdbf6e05
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-01-26 12:06:58 -0500

    Add missing ram mocks

--- tests/src/mocks/include/ram.mock.h
+++ tests/src/mocks/include/ram.mock.h
@@ -9,6 +9,8 @@ extern "C" {
 
 DECLARE_FAKE_VALUE_FUNC(int, ram_init, ram_table_t *, void *);
 DECLARE_FAKE_VALUE_FUNC(int, ram_region_add_memory, uint64_t, uint64_t);
+DECLARE_FAKE_VALUE_FUNC(size_t, ram_free_pages);
+DECLARE_FAKE_VALUE_FUNC(size_t, ram_max_pages);
 DECLARE_FAKE_VALUE_FUNC(uint32_t, ram_page_alloc);
 DECLARE_FAKE_VALUE_FUNC(uint32_t, ram_page_palloc);
 DECLARE_FAKE_VALUE_FUNC(int, ram_page_free, uint32_t);
--- tests/src/mocks/src/ram.mock.c
+++ tests/src/mocks/src/ram.mock.c
@@ -3,6 +3,8 @@
 
 DEFINE_FAKE_VALUE_FUNC(int, ram_init, ram_table_t *, void *);
 DEFINE_FAKE_VALUE_FUNC(int, ram_region_add_memory, uint64_t, uint64_t);
+DEFINE_FAKE_VALUE_FUNC(size_t, ram_free_pages);
+DEFINE_FAKE_VALUE_FUNC(size_t, ram_max_pages);
 DEFINE_FAKE_VALUE_FUNC(uint32_t, ram_page_alloc);
 DEFINE_FAKE_VALUE_FUNC(uint32_t, ram_page_palloc);
 DEFINE_FAKE_VALUE_FUNC(int, ram_page_free, uint32_t);
@@ -12,6 +14,8 @@ DEFINE_FAKE_VALUE_FUNC(size_t, ram_max_pages);
 void reset_ram_mock() {
     RESET_FAKE(ram_init);
     RESET_FAKE(ram_region_add_memory);
+    RESET_FAKE(ram_free_pages);
+    RESET_FAKE(ram_max_pages);
     RESET_FAKE(ram_page_alloc);
     RESET_FAKE(ram_page_palloc);
     RESET_FAKE(ram_page_free);
