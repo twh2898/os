5220165ab5dbc83a8a0b9e80a6168bfe042d062f
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-03-15 19:32:12 -0400

    Replace function exclude with start / stop exclude to support gha compiler

--- src/cpu/i386/src/mmu.c
+++ src/cpu/i386/src/mmu.c
@@ -123,6 +123,8 @@ uint32_t mmu_table_get_flags(mmu_table_t * table, size_t i) {
     return table->entries[i] & MASK_FLAGS;
 }
 
-void mmu_flush_tlb(uint32_t vaddr) { // GCOVR_EXCL_FUNCTION
+// GCOVR_EXCL_START
+void mmu_flush_tlb(uint32_t vaddr) {
     asm volatile("invlpg (%0)" ::"r"(vaddr) : "memory");
 }
+// GCOVR_EXCL_STOP
