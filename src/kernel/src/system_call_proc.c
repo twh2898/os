dadb884c1c86a4089ede98492f9aba6af3e1f7e1
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-03-17 08:28:46 -0400

    Keep args as pointer

--- src/kernel/src/system_call_proc.c
+++ src/kernel/src/system_call_proc.c
@@ -17,8 +17,8 @@ int sys_call_proc_cb(uint16_t int_no, void * args_data, registers_t * regs) {
         case SYS_INT_PROC_EXIT: {
             struct _args {
                 uint8_t code;
-            } args = *(struct _args *)args_data;
-            printf("Proc exit with code %u\n", args.code);
+            } * args = (struct _args *)args_data;
+            printf("Proc exit with code %u\n", args->code);
             kernel_exit();
         } break;
 
@@ -26,9 +26,9 @@ int sys_call_proc_cb(uint16_t int_no, void * args_data, registers_t * regs) {
             struct _args {
                 uint8_t      code;
                 const char * msg;
-            } args = *(struct _args *)args_data;
-            printf("Proc abort with code %u\n", args.code);
-            puts(args.msg);
+            } * args = (struct _args *)args_data;
+            printf("Proc abort with code %u\n", args->code);
+            puts(args->msg);
             kernel_exit();
         } break;
 
@@ -37,18 +37,18 @@ int sys_call_proc_cb(uint16_t int_no, void * args_data, registers_t * regs) {
                 const char * msg;
                 const char * file;
                 unsigned int line;
-            } args = *(struct _args *)args_data;
+            } * args = (struct _args *)args_data;
             vga_color(VGA_FG_WHITE | VGA_BG_RED);
             vga_puts("[PANIC]");
-            if (args.file) {
+            if (args->file) {
                 vga_putc('[');
-                vga_puts(args.file);
+                vga_puts(args->file);
                 vga_puts("]:");
-                vga_putu(args.line);
+                vga_putu(args->line);
             }
-            if (args.msg) {
+            if (args->msg) {
                 vga_putc(' ');
-                vga_puts(args.msg);
+                vga_puts(args->msg);
             }
             vga_cursor_hide();
             asm("cli");
@@ -60,8 +60,8 @@ int sys_call_proc_cb(uint16_t int_no, void * args_data, registers_t * regs) {
         case SYS_INT_PROC_REG_SIG: {
             struct _args {
                 signals_master_cb_t cb;
-            } args = *(struct _args *)args_data;
-            tmp_register_signals_cb(args.cb);
+            } * args = (struct _args *)args_data;
+            tmp_register_signals_cb(args->cb);
         } break;
 
         case SYS_INT_PROC_GETPID: {
@@ -77,18 +77,18 @@ int sys_call_proc_cb(uint16_t int_no, void * args_data, registers_t * regs) {
         case SYS_INT_PROC_QUEUE_EVENT: {
             struct _args {
                 ebus_event_t * event;
-            } args = *(struct _args *)args_data;
+            } * args = (struct _args *)args_data;
 
-            ebus_push(get_kernel_ebus(), args.event);
+            ebus_push(get_kernel_ebus(), args->event);
         } break;
 
         case SYS_INT_PROC_YIELD: {
             struct _args {
                 int filter;
-            } args = *(struct _args *)args_data;
+            } * args = (struct _args *)args_data;
 
             process_t * proc = get_current_process();
-            process_yield(proc, args.filter);
+            process_yield(proc, regs->esp, args->filter);
         };
     }
 
